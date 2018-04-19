d4d93530639d2de8a8263976d9a531a172a7670b
aslan28 <38540070+aslan28@users.noreply.github.com>
2018-04-19 20:35:13 +0100

    Update Van.java

--- Van.java
+++ Van.java
@@ -4,7 +4,7 @@ public class Van extends Vehicle {
 	private String make, model;
 	private int year;
 
-	public Car(String manufacturer, String modelVariant, int dateOfReg, boolean isCar, boolean isMotorcycle, boolean isVan)
+	public Van(String manufacturer, String modelVariant, int dateOfReg, boolean isCar, boolean isMotorcycle, boolean isVan)
 	{
 		super(isCar, isMotorcycle,isVan);
 		make = manufacturer;
@@ -20,12 +20,4 @@ public class Van extends Vehicle {
 	public String toString() {
 		return (make + " " + model + " " + year);
 	}
-
-	public static void main(String[] args) {
-		Car car1 = new Car("Ford", "Focus", 2005);
-		Car car2 = new Car("BMW", "116i", 2006);
-
-		System.out.println(car1);
-		System.out.println(car2);
-	}// end of main method
 }

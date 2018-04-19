66a5ff20a399a51ef1c5ffd61758d00cd85a0d65
D.R <darpan.rai@email.com>
2018-04-19 21:08:18 +0100

    update 1
    1. removed the redundant fields
    2. removed the unwanted fields/variables from the constructor
    3.added the getter method for the field

--- Van.java
+++ Van.java
@@ -1,23 +1,27 @@
 public class Van extends Vehicle {
 
-	// private variables to store the attributes of a car
-	private String make, model;
-	private int year;
+	// private variables to store the attributes of a van
+	private String gearType="";
+	//private int year;
 
-	public Van(String manufacturer, String modelVariant, int dateOfReg, boolean isCar, boolean isMotorcycle, boolean isVan)
+	public Van(int id, String manufacturer, String gearType, boolean isCar, boolean isMotorcycle, boolean isVan)
 	{
-		super(isCar, isMotorcycle,isVan);
-		make = manufacturer;
-		model = modelVariant;
-		year = dateOfReg;
+		super(id, manufacturer, isCar, isMotorcycle,isVan);
+		this.gearType = gearType;
+		
 	} // end of constructor
 
+	public String getGearType()
+	{
+		return gearType;
+	}
 	/**
 	 * This method returns a String to represent a car
 	 * 
 	 * @return
-	 */
-	public String toString() {
-		return (make + " " + model + " " + year);
-	}
+	 *
+	*public String toString() {
+	*	return (make + " " + model + " " + year);
+	*}
+	*/
 }
